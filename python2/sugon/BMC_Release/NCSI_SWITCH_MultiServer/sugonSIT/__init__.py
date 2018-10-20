bcc5b59289c5a2b8517e9cc5e39236419ac13fd6
SamtechTianjin <43158803+SamtechTianjin@users.noreply.github.com>
2018-10-20 11:52:43 +0800

    删除BMC测试代码

--- python2/sugon/BMC_Release/NCSI_SWITCH_MultiServer/sugonSIT/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-import sys
-import os
-cwd = os.path.dirname(os.path.abspath(__file__))
-if not cwd in sys.path:
-    sys.path.append(cwd)
-print 'Package [tecsw] Loaded...'
-__all__ = ['config_handler', 'console_tool', 'debugport']
